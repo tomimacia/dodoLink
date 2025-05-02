d92f469a925124b18a7b25736b87225d34f2fd9f
Tomas <tomimacia@gmail.com>
2025-05-02 17:50:14 -0300

    New loading screen bg

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -25,7 +25,7 @@ const LoadingScreen = () => {
           width: '100%',
           height: '100%',
           minHeight: '100vh',
-          backgroundColor: '#020d2e',
+          backgroundColor: '#092991',
           top: 0,
           zIndex: 20000,
         }}

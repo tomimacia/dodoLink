0116fe9ef2e2c425f43bb598c57e8262d2859c62
Tomas <tomimacia@gmail.com>
2025-05-22 22:49:45 -0300

    Deletd file

--- components/reservas/CargaPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CobrarForm from '../movimientos/CobraFormComps/CobrarForm';
-
-const CargaPage = () => {
-  return <CobrarForm onClose={() => {}} />;
-};
-
-export default CargaPage;
